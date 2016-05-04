15ce2bc141e7f08a04ad99c41ed28147410cbef6
Steven Frieson <sfrieson@gmail.com>
2016-05-04 16:23:38 -0400

    Adds some tests for primitives and objects

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -6,23 +6,109 @@ describe("Basic functionality:", function(){
       expect(anError).toThrowError(TypeError, "You must supply two arguments to diff.");
     });
   });
-  describe("When two objects are the same,", function(){
-    var j5 = {jackson: 5};
-    var songs = ["ABC", "I'll Be There", "Rockin' Robin"];
-    var members = 5;
-    var name = "Jackson 5";
-    var overplayed = false;
-    var expirationDate = null;
+});
 
-    it("should return true.", function(){
+//Primitives
+describe("Primitives:", function(){
+  describe("When arguments are equal (except undefined)", function(){
+    var name = "Jackson 5",
+        members = 5,
+        overplayed = false,
+        royalties = null,
+        afro = Symbol('afro'),
+        label;
+    it("should return false.", function(){
       expect(
-        diff(j5,j5),
-        diff(songs,songs),
-        diff(members,members),
-        diff(name,name),
-        diff(overplayed,overplayed),
-        diff(expirationDate,expirationDate)
-      ).toEqual(true);
+        diff(members,5),
+        diff(name,"Jackson 5"),
+        diff(overplayed, false),
+        diff(royalties,null),
+        diff(afro,afro)
+      ).toEqual(false);
+      var anError = function(){ diff(label, undefined); };
+      expect(anError).toThrowError();
+    });
+  });
+
+  describe("When the arguments are the same type and values are different", function(){
+    it("should return the newer argument", function(){
+      expect(diff(4,5)).toEqual(5);
+      expect(diff("Hi","Hello")).toEqual("Hello");
+      expect(diff(false,true)).toEqual(true);
+      var sym = Symbol(5);
+      expect(diff(Symbol(5),sym)).toEqual(sym);
+    });
+  });
+  describe("When the arguments are the different types", function(){
+    it("should return the newer argument", function(){
+      expect(diff(5,null)).toEqual(null);
     });
   });
 });
+
+//Objects
+describe("Objects:", function(){
+  var obj1 = {name: "John Smith", age: 25},
+      obj2 = {name: "John Smith", age: 26, nationality: "British"},
+      nestObj1 = {first: "John", last: "Smith", kids: ["John", "Sam", "Sally"], address:{num: 5, street: "Easy St", zip:90210}},
+      nestObj2 = {first: "Jane", maiden: "Doe", last: "Smith", kids: ["Sam", "Sally"], address:{num: 5, street: "Easy St", zip:90210}},
+      mixed1,
+      mixed2,
+      arr1 = ["apple", "banana", "mango", "kiwi", "grapes", "lemon"],
+      arr2 = ["apple", "banana", "pineapple", "Mango", "lemon", "grapes"],
+      fn1 = function add(a, b){return a+b;},
+      fn2 = function subtract(a,b){return a-b;},
+      fnAnon = function(){return "Jasmine Testing";};
+
+  describe("When objects are the same reference,", function(){
+    var j5 = {jackson: 5},
+        songs = ["ABC", "I'll Be There", "Rockin\' Robin"],
+        sing = function(){return "She's a dance, dance, dance, dancing machine.";};
+
+    it("should return false.", function(){
+      expect(
+        diff(j5,j5),
+        diff(songs,songs),
+        diff(sing,sing)
+      ).toEqual(false);
+    });
+  });
+  describe("When objects are same type", function(){
+    it("should return false.", function(){
+      var result = diff(obj1,obj2);
+      expect( result.changed.hasOwnProperty('age') ).toEqual(true);
+    });
+  });
+});
+
+//Options
+describe("Options:",function(){
+  describe("undefined: When undefined arguments are passed ", function(){
+    it("should not throw a TypeError, and should check for primitive equality", function(){
+      var obj;
+      expect(diff(obj, undefined, {undefined:true})).toEqual(false);
+      obj = {jackson:5};
+      expect(diff(obj, undefined, {undefined:true})).toBe(undefined);
+      expect(diff(undefined, obj, {undefined:true})).toBeTruthy();
+    });
+  });
+  describe("difference: When two numbers are passed", function(){
+    it("should return the difference from the old to the new",function(){
+      expect(diff(5,6, {difference:true})).toEqual(1);
+      expect(diff(5,4, {difference:true})).toEqual(-1);
+    });
+  });
+});
+//options, not testable...
+// describe("When options are", function(){
+//   describe("not included,", function(){
+//     it("should fill all options with false values", function(){
+//
+//     });
+//   });
+//   describe("included,", function(){
+//     it("should fill the remaining options with false values", function(){
+//
+//     });
+//   });
+// });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,68 @@
 var diff = function (a, b, opt) {
-  if (a === undefined || b === undefined ) throw new TypeError("You must supply two arguments to diff.");
+  var options = ['undefined'];
+  opt = addOptions(opt, options, false);
+  var primitiveRegEx = /string|number|boolean|null|undefined|symbol/i;
+  var results = false;
+  // Add on missing options
 
-  //primitive checking
-  if(typeof a === "boolean" || typeof a === "number" || typeof a === "string" || a.constructor === "date" || )
-  if (a === b) return true;
+  if ((a === undefined || b === undefined) && !opt['undefined']) throw new TypeError("You must supply two arguments to diff.");
 
-  return false;
+  // Primitive equality or same reference  check
+  if (a === b) return results;
 
+  //Same Types
+  if(typeof a === typeof b){
+    if(primitiveRegEx.test(typeof a)) results = primitiveDiff.call(null, a, b, opt);
+    else results = objectDiff.call(null, a, b, opt);
+
+  //Different Types
+  } else {
+    if(primitiveRegEx.test(typeof a)) results = b;
+
+    //TODO: More cases before this
+    else results = b;
+  }
+
+  return results;
+
+};
+
+function primitiveDiff (a, b, opt){
+  var options = ["difference"];
+  opt = addOptions(opt, options, false);
+  if(opt.difference && /number/.test(typeof a)) return b-a;
+  return b;
 }
 
-module.exports = diff;
+function objectDiff (a, b, opt){
+  var options = ["difference"];
+  opt = addOptions(opt, options, false);
+  var results = {
+    added:[],
+    removed:[],
+    changed: {}
+  };
 
-function p(n){k=i=0;while(k<n)if(++i!=(i+"").split('').reverse().join(''))++k;return i}
-(n=>{k=i=0;while(k<n)if(++i!=(i+"").split('').reverse().join(''))++k;return i})(prompt())
-(n,k,i=>while(k<n)if(++i!=(i+"").split('').reverse().join(''))++k; i)(prompt(),0,0)
+  var properties = {};
+  var prop;
+  for(prop in a) properties[prop] = true;
+  for(prop in b) properties[prop] = true;
+  for(prop in properties) {
+    if(a.hasOwnProperty(prop) && b.hasOwnProperty(prop)) {
+      var result = diff.call(null, a[prop], b[prop], opt);
+      if (result) results.changed[prop] = result;
+    }
+    else {
+      if(a.hasOwnProperty(prop)) results.removed.push(prop);
+      if(b.hasOwnProperty(prop)) results.added.push(prop);
+    }
+  }
+  return results;
+}
+
+function addOptions (object, optArr, defaultValue){
+  object = object || {};
+  optArr.forEach(function(option){ object[option] = object[option] || defaultValue; });
+  return object;
+}
+module.exports = diff;
